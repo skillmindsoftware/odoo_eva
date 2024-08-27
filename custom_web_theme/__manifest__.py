d048b60d081fb20d6e652f87ceeb649f1433d0f9
Edwin Mutua <odoo@softiqtechnologies.com>
2024-08-27 12:59:34 +0300

    removed product_card_snippet

--- custom_web_theme/__manifest__.py
+++ custom_web_theme/__manifest__.py
@@ -15,9 +15,9 @@
     'data': [
         'views/layout.xml',
         # 'views/homepage.xml',
-        'views/snippets/snippets.xml',
+        # 'views/snippets/snippets.xml',
     ],
-    
+
     'assets': {
         'web._assets_primary_variables': [
             'custom_web_theme/static/src/scss/primary_variables.scss',
